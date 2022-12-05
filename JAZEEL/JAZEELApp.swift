f69c2dbcbf7729ff56d026dbf8a32913aa844242
RandAlMuhanna <Rand.y.almuhanna@gmail.com>
2022-12-05 21:02:07 +0300

    Initial Commit

---
+++ JAZEEL/JAZEELApp.swift
@@ -0,0 +1,17 @@
+//
+//  JAZEELApp.swift
+//  JAZEEL
+//
+//  Created by Rand AlMuhanna on 11/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct JAZEELApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
