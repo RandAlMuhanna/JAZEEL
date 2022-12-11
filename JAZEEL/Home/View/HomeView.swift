002e098af239ebeb8723fed4294119cb52be4649
Ashwaq3 <eng.ashwaq.h.a@hotmail.com>
2022-12-11 12:19:44 +0300

    Splash Screen

---
+++ JAZEEL/Home/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  JAZEEL
+//
+//  Created by Ashwaq Azan on 17/05/1444 AH.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
